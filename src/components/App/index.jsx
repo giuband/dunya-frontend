7e4ad43c95f8ae46ce32849efa9eab32deec931f
giuband <giuseppe.bandiera@live.com>
2016-09-08 10:06:24 +0200

    App skeleton

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,3 +1,22 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from '../../store';
+import Navbar from '../Navbar';
+import LoginModal from '../LoginModal';
+import Sidebar from '../Sidebar';
+import Results from '../Results';
+import '../../stylesheets/App.scss';
 
-export default () => <div>Hello world!</div>;
+const store = configureStore();
+const App = () => (
+  <Provider store={store}>
+    <div>
+      <Navbar />
+      <Sidebar />
+      <Results />
+      <LoginModal />
+    </div>
+  </Provider>
+);
+
+export default App;
---
+++ src/reducers/sidebar.js
@@ -0,0 +1,8 @@
+const sidebar = (state = {}, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default sidebar;
