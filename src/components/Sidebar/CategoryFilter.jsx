2bbd6005a2aa3fef395941c4da665fd4d7c6ccd2
giuband <giuseppe.bandiera@live.com>
2016-09-09 10:57:00 +0200

    Mock data

--- src/components/Sidebar/CategoryFilter.jsx
+++ src/components/Sidebar/CategoryFilter.jsx
@@ -12,8 +12,19 @@ const CategoryFilter = (props) => (
         {props.title}
         <i className="fa fa-lg fa-plus-circle" aria-hidden />
       </summary>
-      {props.data.map(entry =>
-        <div key={entry.name} className="CategoryFilter__category-entry">{entry.name}</div>)}
+      <section>
+        <div className="CategoryFilter__category-selected-entries-list">
+          {`No ${props.title} selected`}
+        </div>
+        <header>Our catalogue</header>
+        <div className="CategoryFilter__category-catalogue">
+          <div className="CategoryFilter__category-all-entries-list">
+          {props.data.map(entry =>
+            <div key={entry.name} className="CategoryFilter__category-entry">{entry.name}</div>)}
+          </div>
+          <div className="CategoryFilter__category-catalogue-search" />
+        </div>
+      </section>
     </details>
   </div>
 );
--- src/components/Sidebar/RefineSearch.jsx
+++ src/components/Sidebar/RefineSearch.jsx
@@ -2,20 +2,37 @@ import React from 'react';
 import CategoryFilter from './CategoryFilter';
 import '../../stylesheets/RefineSearch.scss';
 
-const artist1 = {
-  name: 'Cool Artist',
-  id: '1',
-  raagas: ['1', '2', '5'],
-  taalas: ['4', '6'],
-  concerts: ['1', '5'],
-};
+const getArtists = (numOfArtists) =>
+  [...Array(numOfArtists).keys()].map(index => ({
+    name: `Cool Artist ${index}`,
+    id: index,
+    concerts: [`${(index % 2) === 0 ? '1' : '2'}`, '5'],
+    raagas: ['1', `${(index % 2) === 0 ? '3' : '5'}`],
+    taalas: [`${(index % 2) === 0 ? '1' : '3'}`, '4'],
+    instruments: [`${(index % 2) === 0 ? '1' : '4'}`, '5'],
+  }));
 
-const receivedData = {
-  artists: [artist1],
-  concerts: [],
-  instruments: [],
-  raagas: [],
-  taalas: [],
+const getConcerts = (concertsIds) =>
+  concertsIds.map((concertID, index) => ({
+    name: `Cool Concert ${index}`,
+    id: concertID,
+    raagas: ['1', `${(index % 2) === 0 ? '3' : '5'}`],
+    taalas: [`${(index % 2) === 0 ? '1' : '3'}`, '4'],
+    instruments: [`${(index % 2) === 0 ? '1' : '4'}`, '5'],
+  }));
+
+const getRefineGenericCategory = (categoryName, categoryIDs) =>
+  categoryIDs.map((id, index) => ({
+    id,
+    name: `Cool ${categoryName} ${index}`,
+  }));
+
+export const receivedData = {
+  artists: getArtists(10),
+  concerts: getConcerts([1, 2, 5]),
+  instruments: getRefineGenericCategory('instrument', [1, 4, 5]),
+  raagas: getRefineGenericCategory('raaga', [1, 3, 5]),
+  taalas: getRefineGenericCategory('taala', [1, 3, 4]),
 };
 
 const RefineSearch = () => (
