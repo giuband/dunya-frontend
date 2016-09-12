bb8fa6ee55ff17056485a3cd10c58ae109bd7c35
giuband <giuseppe.bandiera@live.com>
2016-09-12 16:31:16 +0200

    Ids as strings

--- src/utils/mockFiltersData.js
+++ src/utils/mockFiltersData.js
@@ -1,20 +1,20 @@
 const getArtists = numOfArtists =>
   [...Array(numOfArtists).keys()].map(index => ({
     name: `Cool Artist ${index}`,
-    id: index,
-    concerts: [parseInt(`${(index % 2) === 0 ? 1 : 2}`, 10), 5],
-    raagas: [1, parseInt(`${(index % 2) === 0 ? 3 : 5}`, 10)],
-    taalas: [parseInt(`${(index % 2) === 0 ? 1 : 3}`, 10), 4],
-    instruments: [parseInt(`${(index % 2) === 0 ? 1 : 4}`, 10), 5],
+    id: `${index}`,
+    concerts: [`${(index % 2) === '0' ? '1' : '2'}`, '5'],
+    raagas: ['1', `${(index % 2) === '0' ? '3' : '5'}`],
+    taalas: [`${(index % 2) === '0' ? '1' : '3'}`, '4'],
+    instruments: [`${(index % 2) === '0' ? '1' : '4'}`, '5'],
   }));
 
 const getConcerts = concertsIds =>
   concertsIds.map((concertID, index) => ({
     name: `Cool Concert ${index}`,
     id: concertID,
-    raagas: [1, parseInt(`${(index % 2) === 0 ? 3 : 5}`, 10)],
-    taalas: [parseInt(`${(index % 2) === 0 ? 1 : 3}`, 10), 4],
-    instruments: [parseInt(`${(index % 2) === 0 ? 1 : 4}`, 10), 5],
+    raagas: ['1', `${(index % 2) === '0' ? '3' : '5'}`],
+    taalas: [`${(index % 2) === '0' ? '1' : '3'}`, '4'],
+    instruments: [`${(index % 2) === '0' ? '1' : '4'}`, '5'],
   }));
 
 const getRefineGenericCategory = (categoryName, categoryIDs) =>
@@ -25,8 +25,8 @@ const getRefineGenericCategory = (categoryName, categoryIDs) =>
 
 export const receivedData = {
   artists: getArtists(10),
-  concerts: getConcerts([1, 2, 5]),
-  instruments: getRefineGenericCategory('instrument', [1, 4, 5]),
-  raagas: getRefineGenericCategory('raaga', [1, 3, 5]),
-  taalas: getRefineGenericCategory('taala', [1, 3, 4]),
+  concerts: getConcerts(['1', '2', '5']),
+  instruments: getRefineGenericCategory('instrument', ['1', '4', '5']),
+  raagas: getRefineGenericCategory('raaga', ['1', '3', '5']),
+  taalas: getRefineGenericCategory('taala', ['1', '3', '4']),
 };
