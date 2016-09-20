cec45f72edf022e136cadb485f4eb420f458a473
giuband <giuseppe.bandiera@live.com>
2016-09-20 12:04:53 +0200

    Correctly configure react hot loader

--- src/index.jsx
+++ src/index.jsx
@@ -4,5 +4,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import App from './components/App';
+import configureStore from './store';
 
-render(<AppContainer><App /></AppContainer>, document.getElementById('app'));
+const store = configureStore();
+
+render(<AppContainer><App store={store} /></AppContainer>, document.getElementById('app'));
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    const AppComponent = require('./components/App').default;
+
+    render(
+      <AppContainer>
+        <AppComponent store={store} />
+      </AppContainer>,
+      document.getElementById('app')
+    );
+  });
+}
