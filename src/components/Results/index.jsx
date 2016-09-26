f4be28696fe7c8b5ad72902dddb30547411ca8e4
giuband <giuseppe.bandiera@live.com>
2016-09-26 14:51:15 +0200

    Improve accessibility

--- src/components/Results/index.jsx
+++ src/components/Results/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import ResultItem from './ResultItem';
 import './Results.scss';
 
 const Results = () => (
-  <div className="Results">
+  <section className="Results">
     <header className="Results__header">
       Results
     </header>
     {/** allRecordings.map((recording, index) =>
       <ResultItem key={index} recording={recording} />) */}
-  </div>
+  </section>
 );
 
 export default Results;
